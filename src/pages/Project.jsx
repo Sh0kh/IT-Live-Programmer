7ea20b9bde0bde9b3b8ee0bb45b291dabbda8a33
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2024-09-03 11:34:14 +0500

    edit

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -2,6 +2,9 @@ import React, { useState, useRef, useEffect } from 'react';
 import { $axios } from '../utils';
 import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const GET_TASKS = gql`
     query GetTasks($employeeId: String!) {
         TaskCommon(SortByEmployeeProjectId: $employeeId) {
@@ -17,13 +20,41 @@ const GET_TASKS = gql`
         }
     }
 `;
+const GET_TEAM_LEADER_TASK = gql`
+    query TaskCommon($ID: String!) {
+    TaskCommon(ProjectId: $ID) {
+      condition
+      tasks {
+        id
+        title
+        description
+        fileUrl
+        endAt
+         employeeProject{
+        employee{
+			name
+        }
+      }
+      }
+    }
+  }
+`
 
 function Project() {
     const [employeeId, setEmployeeId] = useState('');
-    const { data: getAllTasks, loading, error } = useQuery(GET_TASKS, {
-        variables: { employeeId },
-        skip: !employeeId,  // Skip query execution until employeeId is set
-    });
+    const [employeeRole, setEmployeeRole] = useState(null) 
+    const { ID } = useParams();
+
+
+    const { data: getAllTasks, loading, error } = useQuery(
+        employeeRole === 'TEAM_LEADER' ? GET_TEAM_LEADER_TASK : GET_TASKS,
+        {
+            variables: employeeRole === 'TEAM_LEADER' ? { ID } : { employeeId },
+            skip: !employeeId && employeeRole !== 'TEAM_LEADER',
+        }
+    );
+
+
     const [items, setItems] = useState({
         column1: [],
         column2: [],
@@ -72,19 +103,19 @@ function Project() {
     const [isMission, setMission] = useState(false);
     const [projectName, setProjectName] = useState('');
 
-    const { ID } = useParams();
     const [cursor, setCursor] = useState('grab');
 
-    const missionModal = () => {
-        setMission(!isMission);
-    };
 
+    const [InfoTask, setInfoTask] = useState([])
+    const missionModal = (task) => {    
+        setMission(!isMission);
+        setInfoTask(task)
+    };
     const handleClickOutside2 = (e) => {
         if (modalRef2.current && !modalRef2.current.contains(e.target)) {
             setMission(false);
         }
     };
-
     useEffect(() => {
         if (isMission) {
             document.addEventListener('mousedown', handleClickOutside2);
@@ -136,7 +167,6 @@ useEffect(() => {
     }
 }, [ID]);
 
-    const [employeeRole, setEmployeeRole] = useState(null) 
 
     const findProject = (data) => {
         const foundObject = data.find(obj => obj.project.id === ID);
@@ -155,8 +185,27 @@ useEffect(() => {
         column3: 'TEST',
         column4: 'CONFIRMED',
     };
-
+    
+  const showErrorToast = () => {
+    toast.error('Sizda bunday huquq yo`q!', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
     const createAndSendTask = (taskId, status) => {
+        // Проверка на запрещенные роли
+        if (status === 'CONFIRMED' && ['FRONT_END', 'BACK_END', 'DESIGNER', 'PROJECT_MANAGER'].includes(employeeRole)) {
+            // alert('У вас нет прав на подтверждение этой задачи.');
+
+            return;
+        }
+
         const url = `/task/updateCondition/${taskId}/${status}`;
         const data = {
             taskId: taskId,
@@ -164,7 +213,7 @@ useEffect(() => {
         };
 
         console.log('Sending PATCH request to:', url);
-    
+
         $axios
             .patch(url, data, {
                 headers: {
@@ -188,18 +237,32 @@ useEffect(() => {
         const draggedItemId = event.dataTransfer.getData('text');
         const newStatus = columnStatusMap[targetColumn];
     
-        // Find the source column where the task is coming from
+        // Проверка на запрещенные роли для статуса CONFIRMED
+        if (newStatus === 'CONFIRMED' && ['FRONT_END', 'BACK_END', 'DESIGNER', 'PROJECT_MANAGER'].includes(employeeRole)) {
+            showErrorToast();
+            return; // Прерываем выполнение функции, чтобы дроп не произошел
+        }
+    
+        // Проверка для TEAM_LEADER
+        if (employeeRole === 'TEAM_LEADER') {
+            if (newStatus === 'PROCESS' || newStatus === 'TEST') {
+                showErrorToast();
+                return; // Прерываем выполнение функции, чтобы дроп не произошел
+            }
+        }
+    
+        // Находим исходную колонку, из которой перетаскивают задачу
         const sourceColumn = Object.keys(items).find((col) =>
             items[col].some((task) => task.id === draggedItemId)
         );
-
-        // If the task is dropped in the same column, do nothing
+    
+        // Если задача перемещается в ту же колонку, ничего не делаем
         if (sourceColumn === targetColumn) return;
     
-        // Create the task and send it to the server
+        // Обновляем задачу и отправляем изменения на сервер
         createAndSendTask(draggedItemId, newStatus);
     
-        // Update the state to move the task to the target column
+        // Обновляем состояние, перемещая задачу в целевую колонку
         setItems((prevItems) => {
             const draggedTask = prevItems[sourceColumn].find((task) => task.id === draggedItemId);
             const updatedSourceItems = prevItems[sourceColumn].filter((task) => task.id !== draggedItemId);
@@ -261,6 +324,7 @@ useEffect(() => {
                                 >
                                     {items[`column${idx + 1}`]?.map((task) => (
                                         <div
+                                            onClick={()=>missionModal(task)}
                                             key={task.id}
                                             id={task.id}
                                             draggable
@@ -277,37 +341,48 @@ useEffect(() => {
                     </div>
                 </div>
             </div>
-            {isMission && (
-                <div className='DeleteModal p-[5px] bg-[#d9d9d9bc] fixed inset-0 flex items-center justify-center'>
+                <div className={`DeleteModal p-[5px] bg-[#d9d9d9bc] fixed inset-0 flex items-center justify-center ${isMission ? 'DeleteModalActive' : ''}`}>
                     <div ref={modalRef2} className='Modal bg-white rounded-[16px] p-[30px] w-[60%] duration-500 ease-in-out overflow-hidden'>
-                        <div className='flex items-center justify-between'>
-                            <h3 className='font-[600] text-[24px] text-[#2D2D2D] font-montserrat'>
-                                Task Actions
-                            </h3>
-                            <button
-                                className='text-[#2D2D2D] text-[24px] font-montserrat'
-                                onClick={missionModal}
-                            >
-                                X
-                            </button>
+                    <div className='flex items-center justify-between'>
+                            <div className='flex items-start gap-[7px]'>
+                                <svg className='text-[28px]' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" d="M11.5 2h-.585A1.5 1.5 0 0 0 9.5 1h-3a1.5 1.5 0 0 0-1.415 1H4.5A1.5 1.5 0 0 0 3 3.5v10A1.5 1.5 0 0 0 4.5 15h7a1.5 1.5 0 0 0 1.5-1.5v-10A1.5 1.5 0 0 0 11.5 2m-5 0h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1 0-1m.852 8.354l-1.5 1.5a.496.496 0 0 1-.706 0l-.5-.5a.5.5 0 0 1 .707-.707l.146.146l1.146-1.146a.5.5 0 0 1 .707.707m0-4l-1.5 1.5a.496.496 0 0 1-.706 0l-.5-.5a.5.5 0 0 1 .707-.707l.146.146l1.146-1.146a.5.5 0 0 1 .707.707" /></svg>
+                                <h1 className='font-montserrat text-[25px] text-[#1F1E30] font-[600]'>
+                                    Vazifa: {InfoTask.title}
+                                </h1>
+                            </div>
+                            <svg  className='cursor-pointer' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 10.586L5.293 3.879a1 1 0 1 0-1.414 1.414L10.586 12l-6.707 6.707a1 1 0 0 0 1.414 1.414L12 13.414l6.707 6.707a1 1 0 0 0 1.414-1.414L13.414 12l6.707-6.707a1 1 0 0 0-1.414-1.414L12 10.586z" /></svg>
                         </div>
-                        <div className='pt-[20px]'>
-                            <button
-                                onClick={() => handleTaskAction('edit')}
-                                className='bg-[#007bff] text-white py-[10px] px-[20px] rounded-[5px] mr-[10px]'
-                            >
-                                Edit Task
-                            </button>
-                            <button
-                                onClick={() => handleTaskAction('delete')}
-                                className='bg-[#dc3545] text-white py-[10px] px-[20px] rounded-[5px]'
-                            >
-                                Delete Task
-                            </button>
+                        <div className='mt-[20px]'>
+                            <div>
+                                <div className='flex items-center gap-[6px]'>
+                                    <svg className='text-[28px]' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20"><path fill="currentColor" d="M7.085 3A1.5 1.5 0 0 1 8.5 2h3a1.5 1.5 0 0 1 1.415 1H14.5A1.5 1.5 0 0 1 16 4.5v4.707A5.5 5.5 0 0 0 10.257 18H5.5A1.5 1.5 0 0 1 4 16.5v-12A1.5 1.5 0 0 1 5.5 3zM8.5 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm3.565 8.442a2 2 0 0 1-1.43 2.478l-.461.118a4.7 4.7 0 0 0 .01 1.016l.35.083a2 2 0 0 1 1.455 2.519l-.126.423q.387.306.835.517l.325-.344a2 2 0 0 1 2.909.002l.337.358q.44-.203.822-.498l-.156-.556a2 2 0 0 1 1.43-2.478l.461-.118a4.7 4.7 0 0 0-.01-1.017l-.349-.082a2 2 0 0 1-1.456-2.52l.126-.421a4.3 4.3 0 0 0-.835-.519l-.324.344a2 2 0 0 1-2.91-.001l-.337-.358a4.3 4.3 0 0 0-.822.497zM14.5 15.5a1 1 0 1 1 0-2a1 1 0 0 1 0 2"></path></svg>
+                                    <h2 className='font-montserrat text-[25px] text-[#1F1E30] font-[600]'>
+                                        Ta’rif
+                                    </h2>
+                                </div>
+                                <div className='block w-full p-[10px]  rounded-[10px] border-[0.5px] border-[#B6BEC3]'>
+                                    <p>
+                                        {InfoTask.description}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='mt-[20px]'>
+                                <div className='flex items-center gap-[6px]'>
+                                    <svg className='text-[28px]' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"></path><path fill="currentColor" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2m0 4a1 1 0 0 0-1 1v5a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V7a1 1 0 0 0-1-1"></path></g></svg>
+                                    <h2 className='font-montserrat text-[25px] text-[#1F1E30] font-[600]'>
+                                        Berilgan vaqt
+                                    </h2>
+                                </div>
+                                <div className='block w-full p-[10px]  rounded-[10px] border-[0.5px] border-[#B6BEC3]'>
+                                    <span>
+                                        {InfoTask?.endAt?.split('T')[0]}
+                                    </span>
+                                </div>
+                            </div>  
                         </div>
                     </div>
                 </div>
-            )}
+              <ToastContainer />
         </div>
     );
 }
